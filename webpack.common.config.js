73450f1789acd2895817767043deb3ba5d7a8873
Mason L <mason.lawlor@me.com>
2017-06-23 17:45:59 +0800

    Removed font building

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -18,12 +18,6 @@ module.exports = {
             test: /\.(jpe?g|png|gif)$/,
             exclude: /(node_modules)/,
             loader: 'url-loader?limit=10000'
-        }, {
-            test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: "url-loader?limit=10000&minetype=application/font-woff"
-        }, {
-            test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-            loader: "file-loader"
         }]
     },
     postcss: function() {
